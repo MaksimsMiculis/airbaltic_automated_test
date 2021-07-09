3652b9a3d178f65f5c054867dfd6c1c74dc13414
MaksimsMiculis <maksims.miculis@gmail.com>
2021-07-09 04:33:26 -0700

    reworked

--- cypress/integration/airbaltic.ts
+++ cypress/integration/airbaltic.ts
@@ -1,10 +1,10 @@
 import { root } from "../objects/airbaltic/mainPage";
 import { userdata } from "../fixtures/userdata";
 
-//Set loal timeout to have possibility to slowdown the test if requred / to film a video for demo
-let localTimeoutMS = 1000;
+//Set load timeout to have possibility to slowdown the test if requred / to film a video for demo
+let localTimeoutMS = 2000;
 
-//To handle an expection
+//To handle an exception
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
 });
@@ -16,86 +16,72 @@ describe("Test for airbaltic.lv", () => {
 
     it('Collect data in a json object and print the final json object in the console', () => {
 
-
+        // Preconditions
         //Step 1 - open the main page
         cy.visit(Cypress.env("env").AIRBALTIC_BASEURL)
         cy.wait(localTimeoutMS)
 
         //Step 2 - close cookie pop-up
-        cy.get('.close').should('be.visible').click()
+        cy.get(root.body.close_cookie_popup).should('be.visible').click()
         cy.wait(localTimeoutMS)
 
         //Step 3 - close low price pop-up
-        cy.get('.right-content > .close-icon').should('be.visible').click()
+        cy.get(root.body.close_low_price_popup).should('be.visible').click()
         cy.wait(localTimeoutMS)
 
         //Step 4 - change language to English
         cy.get(root.body.click_language).should('be.visible').click()
         cy.get(root.body.select_language).click()
-
-
-        //Press Login button
-        cy.get('.new > .header-wrapper > .label').click()
         cy.wait(localTimeoutMS)
 
-        //Enter test account credentials
-        cy.get('.email > input').click().type(userdata.body.user1login)
-        cy.get('.password > input').click().type(userdata.body.user1password)
+
+        //Step 5 - press Login button
+        cy.get(root.body.click_login).click()
+        cy.wait(localTimeoutMS)
+
+        //Step 6 - enter test account credentials
+        cy.get(root.body.email_input_click).click().type(userdata.body.user1login)
+        cy.get(root.body.email_input_password_click).click().type(userdata.body.user1password)
+        cy.wait(localTimeoutMS)
         //Subbmit
-        cy.get('.login-form > .btn > button').click()
+        cy.get(root.body.login_submit_button).click()
 
 
-        //--- Main test procedure body ---
-        cy.get(root.body.click_language).should('be.visible').click()
-        cy.wait(localTimeoutMS)
+        // Main test procedure body
 
-        cy.get(root.body.select_language).click()
-        cy.wait(localTimeoutMS)
-
-        //Step 5 - select 'one-way flight'
+        //Step 7 - select 'one-way flight'
         cy.get(root.body.oneway_dropdown).click()
         cy.wait(localTimeoutMS)
-
         cy.get(root.body.oneway_select).click()
         cy.wait(localTimeoutMS)
 
-        //Step 6 - input From* 'London (LON))'
-        cy.get(root.body.book_flight_origin_input).click().wait(1000).type('London (LON){enter}')//.should('contain', 'London')
+        //Step 8 - input From* 'London (LON)'
+        cy.get(root.body.book_flight_origin_input).click().wait(500).type('London (LON){enter}')
         cy.wait(localTimeoutMS)
 
-        //Step 7- input To* 'Riga (RIX))'
-        cy.get(root.body.book_flight_destination_input).click().wait(1000).type('Riga (RIX){enter}')//.should('contain', 'Riga')
+        //Step 9- input To* 'Riga (RIX)'
+        cy.get(root.body.book_flight_destination_input).click().wait(500).type('Riga (RIX){enter}')
         cy.wait(localTimeoutMS)
 
-        //Step 8 - press button 'Select dates'
+        //Step 10 - press button 'Select dates'
         cy.get(root.body.button_select_dates).click()
         cy.wait(localTimeoutMS)
         //redirecting to the next page
 
 
-        //Select depart date
+        //Step 11 - select departure date
         cy.scrollTo('center')
-        cy.get('.calendar_072021 > .calendar__table > tbody > :nth-child(4) > :nth-child(5) > div > [data-testid=day-container] > .calendar__day-number').click({ force: true })
+        cy.get(root.body.select_departure_date).click({ force: true })
         cy.wait(localTimeoutMS)
 
-        cy.log('We are here!')
-
-        //Click check-box "I am not a robot"
-        //cy.get('.flightlock__checkbox').click()
-        cy.wait(localTimeoutMS)
-        //redirecting to the next page
-
-        //BECAUSE OF THE CAPCHA LET'S TAEK SUGGESTED FLYGHTS FROM THE MAIN PAGE
-        //Scroll Top
+        //BECAUSE OF THE CAPTCHA LET'S TAKE SUGGESTED FLIGHTS FROM THE MAIN PAGE
+        //Step 12 - scroll to Top
         cy.scrollTo('top')
-        //Click Logo
-        cy.get('.logo__img').click()
-        //cy.get('.header-logo-wrap > a').click()
+        //Step 13 - click Logo, navigate to home page
+        cy.get(root.body.click_header_logo).click()
         cy.wait(localTimeoutMS)
 
-
-
-        //Scroll 
+        //Step 14 - scroll to center
         cy.scrollTo('center')
 
         //Define required variables
@@ -103,27 +89,26 @@ describe("Test for airbaltic.lv", () => {
         let arrive;
         let price;
 
-        //Loop throught all records returned
+        //Step 15 - Loop throught all records returned
         let counter = 4;
         // for (let i = 1; i < counter; i++) {
-        cy.get(':nth-child(2) > .wrap > .item-wrapper > .VueCarousel > .VueCarousel-wrapper > .VueCarousel-inner > .VueCarousel-slide-active .banner-item').each((theElement, row) => {
+        cy.get(root.body.get_banner).each((theElement, row) => {
+
             //Get suggested flights
             //Get Depart
-            // cy.get(':nth-child(2) > .wrap > .item-wrapper > .VueCarousel > .VueCarousel-wrapper > .VueCarousel-inner > .VueCarousel-slide-active > :nth-child('+counter+') > .info > .content > .left-block > .destination').then((theElement) => {
-            // bannerElement.find('.info > .content > .left-block > .destination').then(
-            depart = theElement.find('.info > .content > .left-block > .destination').text();
+            depart = theElement.find(root.body.find_destination).text();
             //not you can use article
             cy.log("depart: " + depart)
             // });
 
             // cy.get(':nth-child(2) > .wrap > .item-wrapper > .VueCarousel > .VueCarousel-wrapper > .VueCarousel-inner > .VueCarousel-slide-active > :nth-child(' + counter + ') > .info > .content > .left-block > .origin').then((theElement) => {
-            arrive = theElement.find('.info > .content > .left-block > .origin').text();
+            arrive = theElement.find(root.body.find_origin).text();
             //not you can use article
             cy.log("arrive: " + arrive)
             // });
 
             // cy.get(':nth-child(2) > .wrap > .item-wrapper > .VueCarousel > .VueCarousel-wrapper > .VueCarousel-inner > .VueCarousel-slide-active > :nth-child(' + counter + ') > .info > .content > .price-block > .number').then((theElement) => {
-            price = theElement.find('.info > .actions > .price-block > .number').text();
+            price = theElement.find(root.body.find_price).text();
             //not you can use article
             cy.log("price: " + price);
 
@@ -132,19 +117,9 @@ describe("Test for airbaltic.lv", () => {
             suggestedflightJson.flightInformation.push({ "depart": depart, "arrive": arrive, "price": price });
 
         }).then(() => {
-            //Print filled in json
+            //Step 16 - Print filled in json
             cy.log(JSON.stringify(suggestedflightJson));
         });
-
-
-
-
-
-        //Logout after each test
-        //cy.get('.new > .header-wrapper > .label').click()
-        //cy.wait(localTimeoutMS)
-        //cy.get('.log-out > a').click()
-        //cy.wait(localTimeoutMS)
 
     });
 });
--- cypress/objects/airbaltic/mainPage.ts
+++ cypress/objects/airbaltic/mainPage.ts
@@ -2,21 +2,30 @@ export const root = {
 
     //Section
     body: {
+
+
+        close_cookie_popup: '.close',
+        close_low_price_popup: '.right-content > .close-icon',
         click_language: '.pre-header-lang > .header-wrapper > .label',
+        click_login: '.new > .header-wrapper > .label',
+        email_input_click: '.email > input',
+        email_input_password_click: '.password > input',
+        login_submit_button: '.login-form > .btn > button',
         select_language: '.languages > .dropdown-list > :nth-child(1) > a',
+
         oneway_dropdown: '.passengers-triptype-wrap > .dropdown-wrap > .dropdown-field',
         oneway_select: '.passengers-triptype-wrap > .dropdown-wrap > .dropdown-list > :nth-child(2)',
-        book_flight_origin_input:'.origin > .input-container > :nth-child(3)',
-        book_flight_destination_input:'.destin > .input-container > :nth-child(3)',
-        button_select_dates:'.button > button',
-        departure_date: '',
-        travel_class: '',
-        economy_class_option: '',
-        find_flights_button: '',
-        sort_filter_button_result_page: '',
-        sortby_button_result_page: '',
-        economy_lo_to_high_option: '',
-        apply_button_result_page: ''
+        book_flight_origin_input: '.origin > .input-container > :nth-child(3)',
+        book_flight_destination_input: '.destin > .input-container > :nth-child(3)',
+        button_select_dates: '.button > button',
+        select_departure_date: '.calendar_072021 > .calendar__table > tbody > :nth-child(4) > :nth-child(5) > div > [data-testid=day-container] > .calendar__day-number',
+        click_header_logo: '.logo__img',
+
+        get_banner: ':nth-child(2) > .wrap > .item-wrapper > .VueCarousel > .VueCarousel-wrapper > .VueCarousel-inner > .VueCarousel-slide-active .banner-item',
+        find_destination: '.info > .content > .left-block > .destination',
+        find_origin: '.info > .content > .left-block > .origin',
+        find_price: '.info > .actions > .price-block > .number',
+
     }
 
 }
